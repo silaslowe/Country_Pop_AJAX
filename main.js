6d87fd6088f728c0fba4ed1109889b73b01bc569
negative1skunk@gmail.com <negative1skunk@gmail.com>
2020-09-01 10:41:57 -0500

    JS updated with error checks

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const number = document.querySelector(".number");
+const number1 = document.querySelector(".number1");
+const number2 = document.querySelector(".number2");
 const btn = document.querySelector(".btn");
 const output = document.querySelector(".output");
 let num = "";
@@ -10,14 +11,22 @@ fetch(url)
   .then((data) => countries.push(...data))
   .catch((error) => console.log(error));
 
-btn.addEventListener("click", numFunc);
+btn.addEventListener("click", calculation);
 
-function numFunc(e, num) {
+function calculation(e) {
   e.preventDefault();
-  num = number.value;
+  num1 = number1.value;
+  num2 = number2.value;
+  reset();
+  errorCheck();
+  parametersCheck();
+
   let region = document.getElementById("region").value;
   let newArr = countries
-    .filter((country) => country.population > num && country.region === region)
+    .filter(
+      (country) =>
+        country.population > num1 && country.population < num2 && country.region === region
+    )
     .map(
       (namepop) =>
         `<li class="outputItem">${namepop.name}: ${numberWithCommas(namepop.population)}</li>`
@@ -26,6 +35,69 @@ function numFunc(e, num) {
   output.innerHTML = newArr;
 }
 
+// Error Check Functions
+
+function errorCheck() {
+  if (!number1.value && !number2.value) {
+    missingInput(number1);
+    missingInput(number2);
+    return;
+  }
+  if (!number1.value) {
+    missingInput(number1);
+    return;
+  }
+  if (!number2.value) {
+    missingInput(number2);
+    return;
+  }
+}
+
+function parametersCheck() {
+  if (num1 > num2) {
+    missingInput(number1);
+    missingInput(number2);
+  }
+}
+
+// Removes green boarder, adds red error border
+
+function missingInput(numberInput) {
+  numberInput.classList.remove("number");
+  numberInput.classList.add("errorbtn");
+}
+
+// Resets the Green border
+
+function reset() {
+  number1.classList.remove("errorbtn");
+  number2.classList.remove("errorbtn");
+  number1.classList.add("number");
+  number2.classList.add("number");
+}
+
+// Adds commas to output Numbers
+
 function numberWithCommas(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
+
+// function errorCheck2() {
+//   if (!number1.value && !number2.value) {
+//     number1.classList.remove("number");
+//     number1.classList.add("errorbtn");
+//     number2.classList.remove("number");
+//     number2.classList.add("errorbtn");
+//     return;
+//   }
+//   if (!number1.value) {
+//     number1.classList.remove("number");
+//     number1.classList.add("errorbtn");
+//     return;
+//   }
+//   if (!number2.value) {
+//     number2.classList.remove("number");
+//     number2.classList.add("errorbtn");
+//     return;
+//   }
+// }
